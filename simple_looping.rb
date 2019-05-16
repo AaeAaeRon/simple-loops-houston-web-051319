093aa5f871688ebf870b0e5ccc7b66ff20fc9309
Aaron B <abriscoe2008@gmail.com>
2019-05-16 22:39:13 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -14,7 +14,7 @@ def times_iterator(number_of_times)
 end
 
 def while_iterator(number_of_times) 
-  count = 0 
+  count = 1 
   while count <= number_of_times 
     puts "Welcome to Flatiron School's Web Development Course!" 
     count += 1 
