a049851f270b5c00a60ce6e0edb43e1dbf8d6202
Aaron B <abriscoe2008@gmail.com>
2019-05-16 22:46:09 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -33,6 +33,6 @@ end
 def for_iterator(number_of_times)
   count = 1..number_of_times
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  for count
+  for count in number_of_times
 end
 
